9831b14494162abcbfad6d4e45cb01476db1f440
Mladen Makivic <makivic.mladen1203@gmail.com>
2022-07-05 17:00:00 -0400

    search form added

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,37 @@
-import React from "react";
+import { useState } from "react";
 
 
 const App = () => {
+  
+  const preset = {
+    search: ''
+  };
+  
+  const [formState, setFormState] = useState(preset);
+
+
+  const handleChange = (e) => {
+    const target = e.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+    setFormState({
+      ...formState,
+      [name]: value
+    });
+  };
+
+
   return (
-    <div>App</div>
+    <div>
+      <input
+        type="text"
+        placeholder="Search"
+        name="search"
+        value={formState.search}
+        onChange={handleChange}
+      />
+    </div>
   );
 };
 

6540e7b8aa84a2de85a0cea99e159aaf9435ef9c
Mladen Makivic <makivic.mladen1203@gmail.com>
2022-07-06 19:46:52 -0400

    able to pin city for forecast

---
+++ src/components/PinnedPlace.js
@@ -0,0 +1,61 @@
+import { useEffect, useState } from "react";
+import { ajax } from "../utils/ajax-adapter";
+import ForecastCard from "./ForecastCard";
+import ForecastToday from "./ForecastToday";
+
+const PinnedPlace = (props) => {
+  const id = props.id;
+  const [result, setResult] = useState({});
+
+  useEffect(() => {
+    console.log('aktiviramo hot search za rijeci: ', id);
+    ajax.getWeatherByCityId(id)
+      .then(obradjeni_response => {
+        if (obradjeni_response) {
+          // sigurno uspio response jer neuspjeli stize kao false
+          console.log('response uspio:', obradjeni_response);
+          setResult(obradjeni_response);
+        }
+      })
+  }, [id]);
+
+  let jsxZeroResult = null;
+  let jsxToday = null;
+  let jsxKartice = null;
+  if (result.list && result.city) {
+
+    jsxToday = (
+      <>
+        <ForecastToday city={result.city} item={result.list[0]} />
+      </>
+    );
+    jsxKartice = result.list.map((item, index) => {
+      if (index === 0) {
+        return null;
+      }
+      return (
+        <ForecastCard key={index} city={result.city} item={item} />
+      );
+    });
+  } else {
+    jsxZeroResult = (
+      <div>No Results</div>
+    );
+  }
+
+  
+
+  return (
+    <div>PINNED CITIES ID: {id}
+
+      <h3>Today</h3>
+      {jsxToday}
+      <h3>5 Day Forecast</h3>
+      <div className="list">
+        {jsxKartice}
+      </div>
+    </div>
+  );
+};
+
+export default PinnedPlace;
--- src/utils/ajax-adapter.js
+++ src/utils/ajax-adapter.js
@@ -28,7 +28,31 @@ export const ajax = {};
 
 ajax.getWeatherSearch = async (q = '') => {
   // q text koji trazimo
-  const url = 'https://community-open-weather-map.p.rapidapi.com/forecast/daily?units=metric&cnt=5&q=' + q;
+  const url = 'https://community-open-weather-map.p.rapidapi.com/forecast/daily?units=metric&cnt=6&q=' + q;
+  try {
+    const response = await axios.get(url, {
+      headers: {
+        'X-RapidAPI-Host': 'community-open-weather-map.p.rapidapi.com',
+        'X-RapidAPI-Key': '51955d2ad7mshc6d85919c22b857p11c3dbjsncb41963d7ec6'
+      }
+    });
+    console.log('response za getWeatherSearch', response);
+    if (response.data && response.data.city && response.data.city.id) {
+      console.log(response.data.city.id);
+      return response.data; // response data nam je cijela prognoza za taj grad
+    } else {
+      console.log('response ne uspijeva1');
+      return false;
+    }
+  } catch (err) {
+    console.log('response ne uspijeva1');
+    return false;
+  }
+};
+
+ajax.getWeatherByCityId = async (id = '') => {
+  // q text koji trazimo
+  const url = 'https://community-open-weather-map.p.rapidapi.com/forecast/daily?units=metric&cnt=6&id=' + id;
   try {
     const response = await axios.get(url, {
       headers: {
